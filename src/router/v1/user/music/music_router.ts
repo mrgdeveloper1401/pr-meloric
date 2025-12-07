b0b5fc3f08803a12ead312b57c9b160f2ecccd13
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-12-07 11:16:27 +0330

    remove increment play_count in api detail music

--- src/router/v1/user/music/music_router.ts
+++ src/router/v1/user/music/music_router.ts
@@ -524,9 +524,6 @@ musicRouter.get(
           },
         },
       };
-      // incress play count
-      getMusic.play_count += 1;
-      await getMusic.save();
 
       return res.status(200).json({
         status: "success",
