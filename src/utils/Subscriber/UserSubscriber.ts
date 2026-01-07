2205299fa378d939f5212f435d0b1eb184c8d511
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-01-07 22:17:02 +0330

    edit create profile user

--- src/utils/Subscriber/UserSubscriber.ts
+++ src/utils/Subscriber/UserSubscriber.ts
@@ -1,8 +1,8 @@
-import { 
-  EntitySubscriberInterface, 
-  EventSubscriber, 
+import {
+  EntitySubscriberInterface,
+  EventSubscriber,
   InsertEvent,
-  DataSource 
+  DataSource,
 } from "typeorm";
 import { User } from "../../entity/User";
 import { Profile } from "../../entity/Profile";
@@ -10,35 +10,34 @@ import { Artist } from "../../entity/Artist";
 
 @EventSubscriber()
 export class UserSubscriber implements EntitySubscriberInterface<User> {
-    constructor(private dataSource: DataSource) {}
-    
-    listenTo(): Function | string {
-        return User;
+  constructor(private dataSource: DataSource) {}
+
+  listenTo(): Function | string {
+    return User;
+  }
+
+  async afterInsert(event: InsertEvent<User>) {
+    try {
+      const profileRepository = event.manager.getRepository(Profile);
+      const artistRepository = event.manager.getRepository(Artist);
+      const user = event.entity;
+
+      if (user.is_artist) {
+        const artistProfile = artistRepository.create({
+          user: user,
+          is_active: true,
+          monthly_listeners: 0,
+        });
+
+        await artistRepository.save(artistProfile);
+        console.log(`Artist profile created for user: ${user.username}`);
+      } else {
+        const profile = new Profile();
+        profile.user = user;
+        await profileRepository.save(profile);
+      }
+    } catch (error) {
+      throw new Error(error);
     }
-
-    async afterInsert(event: InsertEvent<User>) {
-        try {
-            const profileRepository = event.manager.getRepository(Profile);
-            const artistRepository = event.manager.getRepository(Artist);
-            const user = event.entity;
-
-            const profile = new Profile();
-            profile.user = user;
-            await profileRepository.save(profile);
-
-            if (user.is_artist) {
-                const artistProfile = artistRepository.create({
-                    user: user,
-                    is_active: true,
-                    monthly_listeners: 0,
-                });
-                
-                await artistRepository.save(artistProfile);
-                console.log(`Artist profile created for user: ${user.username}`);
-            }
-
-        } catch (error) {
-            throw error;
-        }
-    }
+  }
 }
